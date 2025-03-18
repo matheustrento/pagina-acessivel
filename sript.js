1fcb1e8ee785d1d30c3f82da67a2a0213c34d668
matheustrento <159184960+matheustrento@users.noreply.github.com>
2025-03-18 11:20:05 -0300

    dia do bb

---
+++ sript.js
@@ -0,0 +1,14 @@
+document. addEventListener('DOMContentLoaded' , function() {
+    const aumentaFonteBotao = document.getElementById('aumenta-fonte')
+    const diminuiFonteBotao = document.getElementById('diminui-fonte')
+
+    let tamanhoAtualFonte= 1;
+
+    aumentaFonteBotao.addEventListener('click' , Function(){
+        tamanhoAtualFonte + = 0,1;
+        document.body.style.fontSize=${}
+    })
+
+
+}
+)
