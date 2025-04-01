1a9177c95f759f6462089a3441d293d39903972b
matheustrento <159184960+matheustrento@users.noreply.github.com>
2025-04-01 11:19:22 -0300

    camelo

--- sript.js
+++ sript.js
@@ -1,14 +1,21 @@
-document. addEventListener('DOMContentLoaded' , function() {
-    const aumentaFonteBotao = document.getElementById('aumenta-fonte')
-    const diminuiFonteBotao = document.getElementById('diminui-fonte')
+document. addEventListener ('DOMContentLoaded', function() {
+    const aumentaFonteBotao = this. getElementByld('aumenta-fonte');
+    const diminuiFonteBotao = this. getElementById('diminuirfonte');
 
-    let tamanhoAtualFonte= 1;
+    let tamanhoAtualdaFonte = 1;
 
-    aumentaFonteBotao.addEventListener('click' , Function(){
-        tamanhoAtualFonte + = 0,1;
-        document.body.style.fontSize=${}
+    aumentaFonteBotao.addEventListener('click',function () {
+        tamanhoAtualdaFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualdaFonte}rem`
+
     })
 
+    diminuiFonteBotao.addEventListener('click',function () {
+        tamanhoAtualdaFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualdaFonte}rem`
 
-}
-)
+    
+
+})
+
+})
